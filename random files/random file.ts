562175a3d8413fd10c5828bb49b9b43919cd1b2f
yaay1010 <67664398+yaay1010@users.noreply.github.com>
2021-07-14 18:25:38 -0400

    Create random file.ts

---
+++ random files/random file.ts
@@ -0,0 +1,6 @@
+const log = "Made a french fry"
+const log2 = "Opened the pan"
+const log3 = "Made a cool python file"
+const log4 = "Went to sleep"
+
+console.log(log, "\n", log2, "\n", log3, "\n", log4)
